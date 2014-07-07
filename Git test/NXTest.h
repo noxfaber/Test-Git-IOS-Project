c0755821acc70851236fae56bc863e8da909de34
noxfaber <dev@noxfaber.de>
2014-07-07 16:47:20 +0200

    added test class

---
+++ Git test/NXTest.h
@@ -0,0 +1,13 @@
+//
+//  NXTest.h
+//  Git test
+//
+//  Created by Gültekin Karakelle on 07.07.14.
+//  Copyright (c) 2014 nox faber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NXTest : NSObject
+
+@end
---
+++ Git test/NXTest.m
@@ -0,0 +1,13 @@
+//
+//  NXTest.m
+//  Git test
+//
+//  Created by Gültekin Karakelle on 07.07.14.
+//  Copyright (c) 2014 nox faber. All rights reserved.
+//
+
+#import "NXTest.h"
+
+@implementation NXTest
+
+@end
